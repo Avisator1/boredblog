07d46d24a6f26801dfb170def4c3538a32342149
Avyukt Ballari <ballariavitiktok@gmail.com>
2023-09-30 11:57:44 -0400

    multi line update

--- app.py
+++ app.py
@@ -213,5 +213,6 @@ def noindex():
     r = Response(response="User-Agent: *\nDisallow: /\n", status=200, mimetype="text/plain")
     r.headers["Content-Type"] = "text/plain; charset=utf-8"
     return r
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
